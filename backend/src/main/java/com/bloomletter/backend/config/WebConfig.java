72b90f1d2a22870c5665aabdbdc223d9bc80b7f8
LAPTOP-OELQH1V6\academia <202511_aw_sato@x-ide.sakura.ne.jp>
2026-05-07 11:46:48 +0900

    トップURLをindex.htmlへリダイレクト

--- backend/src/main/java/com/bloomletter/backend/config/WebConfig.java
+++ backend/src/main/java/com/bloomletter/backend/config/WebConfig.java
@@ -5,10 +5,16 @@ import java.nio.file.Paths;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addRedirectViewController("/", "/index.html");
+  }
+
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry
