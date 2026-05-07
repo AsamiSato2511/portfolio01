dedf55d87be90c66bfc29f2c0e3af7b590c0f01b
LAPTOP-OELQH1V6\academia <202511_aw_sato@x-ide.sakura.ne.jp>
2026-05-07 13:46:50 +0900

    トップURLから管理画面へ遷移

--- backend/src/main/java/com/bloomletter/backend/config/WebConfig.java
+++ backend/src/main/java/com/bloomletter/backend/config/WebConfig.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addViewControllers(ViewControllerRegistry registry) {
-    registry.addRedirectViewController("/", "/index.html");
+    registry.addRedirectViewController("/", "/admin/products");
     registry.addRedirectViewController("/admin", "/admin/login");
     registry.addRedirectViewController("/admin/", "/admin/login");
     registry.addViewController("/admin/login").setViewName("forward:/admin/login.html");
